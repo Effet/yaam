62660ab44b5e3d08ff5c671cda802d7d1796c988
Effet <nesuadark@gmail.com>
2018-03-08 22:34:53 +0800

    add project skeleton

---
+++ yaam-web/src/main/java/io/effet/yaam/web/YaamWebApplication.java
@@ -0,0 +1,12 @@
+package io.effet.yaam.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaamWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaamWebApplication.class, args);
+	}
+}
